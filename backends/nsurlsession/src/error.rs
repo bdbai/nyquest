1dd527648004115fc2e16164ca52cd650428471a
bdbai <bdbaiapp@163.com>
2025-04-30 23:57:37 +0800

    nsurlsession timedout error

--- backends/nsurlsession/src/error.rs
+++ backends/nsurlsession/src/error.rs
@@ -2,7 +2,7 @@ use std::io::{self, ErrorKind};
 
 use nyquest_interface::{Error as NyquestError, Result as NyquestResult};
 use objc2::rc::{autoreleasepool, Retained};
-use objc2_foundation::NSError;
+use objc2_foundation::{NSError, NSURLErrorTimedOut};
 
 pub(crate) trait IntoNyquestResult<T> {
     fn into_nyquest_result(self) -> NyquestResult<T>;
@@ -11,6 +11,9 @@ pub(crate) trait IntoNyquestResult<T> {
 impl<T> IntoNyquestResult<T> for Result<T, Retained<NSError>> {
     fn into_nyquest_result(self) -> NyquestResult<T> {
         self.map_err(|e| {
+            if e.code() == NSURLErrorTimedOut {
+                return NyquestError::RequestTimeout;
+            }
             let msg =
                 autoreleasepool(|pool| unsafe { e.localizedDescription().to_str(pool).to_owned() });
             NyquestError::Io(io::Error::new(
